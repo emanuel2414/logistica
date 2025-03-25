2cec6458fa9f3c5e507ca2e8478c7cc3aaf44f44
DiegoRuiiz05 <149976414+DiegoRuiiz05@users.noreply.github.com>
2025-03-24 22:26:02 -0500

    Delete ReporteRecepcionRepository.java

--- ReporteRecepcionRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniajc.visionarios.repository;
-
-import co.edu.uniajc.visionarios.model.ReporteRecepcion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReporteRecepcionRepository extends JpaRepository<ReporteRecepcion, Long> {
-}
